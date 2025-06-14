66e51fc983bbf7ffe7789eb0ba5531b9ebdfb762
Jordan Hilsman <jordanhilsman@gmail.com>
2025-06-14 19:43:47 -0400

    GUI now allows for color channel manipulation & image resetting.

--- src/gui/glimmr_gui.py
+++ src/gui/glimmr_gui.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QPixmap, QImage
 
 from PyQt5.QtWidgets import (
     QApplication,
+    QHBoxLayout,
     QPushButton,
     QCheckBox,
     QComboBox,
@@ -35,21 +36,75 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Welcome to Glimmr!")
         self.start_pos, self.end_pos = QPoint(0,0), QPoint(0,0)
         self.add_channel_zero = 0
+        self.b0 = False
+        self.b1 = False
+        self.b2 = False
+        self.r0 = False
+        self.red_btn = QPushButton("RED")
+        self.green_btn = QPushButton("GREEN")
+        self.blue_btn = QPushButton("BLUE")
+        self.reset_btn = QPushButton("RESET IMAGE")
+        self.color_channel = None
+        for btn in [self.red_btn, self.green_btn, self.blue_btn, self.reset_btn]:
+            btn.setCheckable(True)
+        self.red_btn.clicked.connect(lambda: self.activate_button(2))
+        self.green_btn.clicked.connect(lambda: self.activate_button(1))
+        self.blue_btn.clicked.connect(lambda: self.activate_button(0))
+        self.reset_btn.clicked.connect(self.reset_image)
         widget = QLabel("Hello")
+        self.label = widget
         font = widget.font()
         font.setPointSize(30)
         widget.setFont(font)
         widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        self.setCentralWidget(widget)
         self.cvImg = cv2.imread('/home/jordan/photos/bmw.jpg')
         self.cvImg[:,:,0] = self.cvImg[:,:,0] + self.add_channel_zero
         self.cvImg[:,:,0][self.cvImg[:,:,0] > 255] = 255
         height, width, channel = self.cvImg.shape
         bytesPerLine = 3 * width
         qImg = QImage(self.cvImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
-
         widget.setPixmap(QPixmap(qImg))
-        self.widget = widget
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.red_btn)
+        button_layout.addWidget(self.green_btn)
+        button_layout.addWidget(self.blue_btn)
+        button_layout.addWidget(self.reset_btn)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.label)
+        main_layout.addLayout(button_layout)
+
+        container = QWidget()
+        container.setLayout(main_layout)
+        self.setCentralWidget(container)
+
+    def activate_button(self, button_number):
+        # Reset all
+        self.b1 = self.b2 = self.b3 = False
+        self.red_btn.setChecked(False)
+        self.blue_btn.setChecked(False)
+        self.green_btn.setChecked(False)
+
+        # Set selected
+        if button_number == 0:
+            self.b1 = True
+            self.blue_btn.setChecked(True)
+        elif button_number == 1:
+            self.b2 = True
+            self.green_btn.setChecked(True)
+        elif button_number == 2:
+            self.b3 = True
+            self.red_btn.setChecked(True)
+        self.color_channel = button_number
+
+    def reset_image(self):
+        self.r0 = False
+        self.reset_btn.setChecked(True)
+        self.cvImg = cv2.imread('/home/jordan/photos/bmw.jpg')
+        height, width, channel = self.cvImg.shape
+        bytesPerLine = 3 * width
+        qImg = QImage(self.cvImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        self.label.setPixmap(QPixmap(qImg))
 
     def mousePressEvent(self, e):
         self.start_pos.setX(e.pos().x())
@@ -60,17 +115,16 @@ class MainWindow(QMainWindow):
         self.end_pos.setY(e.pos().y())
         self.add_channel_zero = self.dist_difference(self.start_pos, self.end_pos)
         print(self.add_channel_zero)
-        updated_img = self.update_img(self.cvImg, self.add_channel_zero)
-        self.widget.setPixmap(QPixmap(updated_img))
+        updated_img = self.update_img(self.cvImg, self.add_channel_zero, self.color_channel)
+        self.label.setPixmap(QPixmap(updated_img))
 
     def dist_difference(self, start_pos, end_pos):
         return((end_pos - start_pos).x())
 
-    def update_img(self, cvImg, acz):
+    def update_img(self, cvImg, acz, cc):
         acz = float(acz)
-        cvImg[:,:,1] = cvImg[:,:,1] + min(acz, 255)
-        #cvImg[:,:,0] = cvImg[:,:,0] + max(0, min(acz, 255))
-        cvImg[:,:,1][cvImg[:,:,1] > 255] = 255
+        cvImg[:,:,cc] = cvImg[:,:,cc] + min(acz, 255)
+        cvImg[:,:,cc][cvImg[:,:,cc] > 255] = 255
         height, width, channel = cvImg.shape
         bytesPerLine = 3 * width
         qImg2 = QImage(cvImg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
--- src/gui/root.py
+++ src/gui/root.py
@@ -1,42 +1,29 @@
 import sys
 
 from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton, QTextEdit, QVBoxLayout,
+QWidget)
 
+
+class BUTTA(QPushButton):
+    def __init__(self, message, channel, parent=None):
+        super().__init__(message, parent)
+        self.button = QPushButton(message)
+        self.button.setCheckable(True)
+        self.channel = channel
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setMouseTracking(True)
-        self.label = QLabel("Click in this window")
-        self.label.setMouseTracking(True)
-        self.setCentralWidget(self.label)
-        self.start_pos = QPoint(0,0)
-        self.end_pos = QPoint(0,0)
-
-
-    def mouseMoveEvent(self, e):
-        self.label.setText("mouseMoveEvent")
-
-    def mousePressEvent(self, e):
-        self.start_pos.setX(e.pos().x())
-        self.start_pos.setY(e.pos().y())
-        self.label.setText("mousePressEvent")
-
-    def mouseReleaseEvent(self, e):
-        self.end_pos.setX(e.pos().x())
-        self.end_pos.setY(e.pos().y())
-        self.label.setText("mouseReleaseEvent")
-        print(self.start_pos, self.end_pos)
-        print(self.distance_traveled(self.start_pos, self.end_pos))
-
-    def mouseDoubleClickEvent(self, e):
-        self.label.setText("mouseDoubleClickEvent")
-
-    def distance_traveled(self, start_pos:QPoint, end_pos:QPoint):
-        start_x, start_y = start_pos.x, start_pos.y
-        end_x, end_y = end_pos.x, end_pos.y
-        return(start_pos - end_pos)
+        layout = QVBoxLayout()
+        button1 = BUTTA("BLUE", 0)
+        button2 = BUTTA("GREEN", 1)
+        button3 = BUTTA("RED", 2)
+        layout.addWidget(button1)
+        layout.addWidget(button2)
+        layout.addWidget(button3)
+        self.setLayout(layout)
+#        self.setCentralWidget(self.button1)
 
 app = QApplication(sys.argv)
 
